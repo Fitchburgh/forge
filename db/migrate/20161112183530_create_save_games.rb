c45e90d52f510f1ac5298b2bf2dcbd9d0712253f
Brandon Fitch <fitchbj@gmail.com>
2016-11-12 13:36:32 -0500

    Models aligned, db:migrate working. FK constraints look accurate

---
+++ db/migrate/20161112183530_create_save_games.rb
@@ -0,0 +1,11 @@
+class CreateSaveGames < ActiveRecord::Migration[5.0]
+  def change
+    create_table :save_games do |t|
+      t.references :game, foreign_key: true
+      t.references :user, foreign_key: true
+      t.json :obj
+
+      t.timestamps
+    end
+  end
+end

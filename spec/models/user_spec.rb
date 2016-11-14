06d87bc769b7e10226c838e8b65674e5b675804f
nsemmler <ndsunc@gmail.com>
2016-11-14 12:08:40 -0500

    added tests for validations of game, map, scene, user models.  deleted collaborator and save game models with no validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should not save user if username not present" do
+      expect { User.create! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "should not save user if username not unique" do
+      expect { should validate_uniqueness_of(:username).case_insensitive }
+    end
+  end
 end

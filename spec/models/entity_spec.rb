bed88f035e82b23843fefc637c0b35504a87a699
nsemmler <ndsunc@gmail.com>
2016-11-14 10:46:34 -0500

    testing for validations for entities, obstacles

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Entity do
+  describe "validations" do
+    it "should not save if the entity does not have a JSON object" do
+      expect {
+        Entity.create!(user_id: 1, scene_id: 1, name: "tree", tags: "['oak', 'tall tree']")
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "should not save if the entity does not have a name" do
+      expect {
+        Entity.create!(user_id: 1, scene_id: 1, obj: "{stuff}", tags: "['oak', 'tall tree']")
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
--- spec/models/obstacle_spec.rb
+++ spec/models/obstacle_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Obstacle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Obstacle do
+  describe "validations" do
+    it "should not save if the obstacle does not have a JSON object" do
+      expect {
+        Obstacle.create!(user_id: 1, scene_id: 1, name: "tree", tags: "['oak', 'tall tree']")
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "should not save if the obstacle does not have a name" do
+      expect {
+        Obstacle.create!(user_id: 1, scene_id: 1, obj: "{stuff}", tags: "['oak', 'tall tree']")
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end

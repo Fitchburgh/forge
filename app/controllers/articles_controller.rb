be4804eb5fb77587b4650225f203889cd39f910f
nsemmler <ndsunc@gmail.com>
2016-11-15 16:24:09 -0500

    removed unnecessary instance variables

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,39 +2,29 @@ require 'pry'
 #
 class ArticlesController < ApplicationController
   def index
-    @backgrounds = Background.all
-    @obstacles = Obstacle.all
-    @entities = Entity.all
-
-    @articles = { 'backgrounds' => @backgrounds, 'obstacles' => @obstacles, 'entities' => @entities }
+    @articles = { 'backgrounds' => Background.all, 'obstacles' => Obstacle.all, 'entities' => Entity.all }
     render :json => @articles
   end
 
   def index_backgrounds
-    @backgrounds = Background.all
-    render :json => @backgrounds
+    render :json => Background.all
   end
 
   def index_obstacles
-    @obstacles = Obstacle.all
-    render :json => @obstacles
+    render :json => Obstacle.all
   end
 
   def index_entities
-    @entities = Entity.all
-    render :json => @entities
+    render :json => Entity.all
   end
 
   def create
     if params[:category] == 'background'
       render json: Background.create_background_article(@background, params)
-
     elsif params[:category] == 'obstacle'
       render json: Obstacle.create_obstacle_article(@obstacle, params)
-
     elsif params[:category] == 'entity'
       render json: Entity.create_entity_article(@entity, params)
-
     else
       halt(404)
     end

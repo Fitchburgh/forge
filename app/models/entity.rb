ce549805c4a7165b20617c59c3f32b141fe3150d
Brandon Fitch <fitchbj@gmail.com>
2016-11-15 16:49:41 -0500

    dont need to to_s a json object.

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -22,7 +22,7 @@ class Entity < ApplicationRecord
 
   def self.new_entity(options)
     Entity.new(
-      obj: options['obj'].to_s, user_id: options['user_id'],
+      obj: options['obj'], user_id: options['user_id'],
       game_id: options['game_id'], name: options['name'].downcase,
       tags: options['tags'].to_s.downcase, public: options['public']
     )

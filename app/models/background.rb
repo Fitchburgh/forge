126530171272aa1db99cbb2fd0f74d196b43fb0e
Brandon Fitch <fitchbj@gmail.com>
2016-11-15 16:09:11 -0500

    Changed downcase from json obj in backgrounds

--- app/models/background.rb
+++ app/models/background.rb
@@ -23,7 +23,7 @@ class Background < ApplicationRecord
 
   def self.new_background(options)
     Background.new(
-      obj: options['obj'].downcase, user_id: options['user_id'],
+      obj: options['obj'], user_id: options['user_id'],
       game_id: options['game_id'], name: options['name'].downcase,
       tags: options['tags'].downcase
     )

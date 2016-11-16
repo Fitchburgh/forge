de3df4c50da85568f92934d631db3a7bec1e4e58
nsemmler <ndsunc@gmail.com>
2016-11-15 20:51:47 -0500

    crappy but working update and delete methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,15 +34,89 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    if params[:category] == 'background'
+      @background = Background.find_by(id: params[:id])
+      if !@background.nil?
 
+        @background.name = params[:name].downcase
+        @background.tags = params[:tags].downcase
+        @background.user_id = params[:user_id]
+        @background.game_id = params[:game_id]
+        @background.obj = params[:obj]
+
+        if @background.save
+          render :json => @background
+        else
+          render :json => { :errors => @background.errors.full_messages }, status: 404
+        end
+      else
+        render :json => { message: 'background is not found' }
+      end
+    elsif params[:category] == 'obstacle'
+      @obstacle = Obstacle.find_by(id: params[:id])
+      if !@obstacle.nil?
+
+        @obstacle.name = params[:name].downcase
+        @obstacle.tags = params[:tags].downcase
+        @obstacle.user_id = params[:user_id]
+        @obstacle.game_id = params[:game_id]
+        @obstacle.obj = params[:obj]
+
+        if @obstacle.save
+          render :json => @obstacle
+        else
+          render :json => { :errors => @obstacle.errors.full_messages }, status: 404
+        end
+      else
+        render :json => { message: 'obstacle is not found' }
+      end
+    elsif params[:category] == 'entity'
+      @entity = Entity.find_by(id: params[:id])
+      if !@entity.nil?
+
+        @entity.name = params[:name].downcase
+        @entity.tags = params[:tags].downcase
+        @entity.user_id = params[:user_id]
+        @entity.game_id = params[:game_id]
+        @entity.obj = params[:obj]
+
+        if @entity.save
+          render :json => @entity
+        else
+          render :json => { :errors => @entity.errors.full_messages }, status: 404
+        end
+      else
+        render :json => { message: 'entity is not found' }
+      end
+    else
+      render :json => { message: 'category not found' }
+    end
   end
 
   def delete
-    @background = Background.find_by(id: params[:id])
-    if @background.nil?
-      render :json => { error: 'background not detected' }, status: 404
+    if params[:category] == 'background'
+      @background = Background.find_by(id: params[:id])
+      if @background.nil?
+        render :json => { error: 'background not detected' }, status: 404
+      else
+        render :json => { message: 'background deleted' } if @background.delete
+      end
+    elsif params[:category] == 'obstacle'
+      @obstacle = Obstacle.find_by(id: params[:id])
+      if @obstacle.nil?
+        render :json => { error: 'obstacle not detected' }, status: 404
+      else
+        render :json => { message: 'obstacle deleted' } if @obstacle.delete
+      end
+    elsif params[:category] == 'entity'
+      @entity = Entity.find_by(id: params[:id])
+      if @entity.nil?
+        render :json => { error: 'entity not detected' }, status: 404
+      else
+        render :json => { message: 'entity deleted' } if @entity.delete
+      end
     else
-      render :json => { message: 'background deleted' } if @background.delete
+      halt(404)
     end
   end
 

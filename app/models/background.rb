18d5a6a2fff7f7a208cb32a69738fcb6710ea803
nsemmler <ndsunc@gmail.com>
2016-11-16 09:29:41 -0500

    re-ordered params in models

--- app/models/background.rb
+++ app/models/background.rb
@@ -18,18 +18,21 @@ class Background < ApplicationRecord
 
   def self.new_background(options)
     Background.new(
-    obj: options[:obj], user_id: options[:user_id],
-    game_id: options[:game_id], name: options[:name].downcase,
-    tags: options[:tags].to_s.downcase, public: options[:public]
+    obj: options[:obj],
+    user_id: options[:user_id],
+    game_id: options[:game_id],
+    public: options[:public],
+    name: options[:name].downcase,
+    tags: options[:tags].to_s.downcase
   )
   end
 
   def self.update_background(var, params)
-    var.name = params[:name].downcase
-    var.tags = params[:tags].downcase
+    var.obj = params[:obj]
     var.user_id = params[:user_id]
     var.game_id = params[:game_id]
-    var.obj = params[:obj]
     var.public = params[:public]
+    var.name = params[:name].downcase
+    var.tags = params[:tags].downcase
   end
 end

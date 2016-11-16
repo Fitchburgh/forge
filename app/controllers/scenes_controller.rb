8b295aa930b0a72f13baa07ec870392bb2f6e121
Brandon Fitch <fitchbj@gmail.com>
2016-11-16 17:24:31 -0500

    Small changes to make json store

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,8 +1,7 @@
 require 'pry'
-# This will really only be needed if game obj load too slowly
+#
 class ScenesController < ApplicationController
   def create
-    binding.pry
     render json: Scene.create_scene(@scene, params), status: 201
   end
 

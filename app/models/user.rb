36e0f7d98ecda82cd7508222b095877139122256
nsemmler <ndsunc@gmail.com>
2016-11-16 16:31:24 -0500

    Redis for users was a terrible idea.  All traces removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,3 @@
 class User < ApplicationRecord
   validates :username, :token, :uid, presence: true, uniqueness: true
-
-  def self.find_user
-    @username = Redis.current.get(@user.username)
-    @token = Redis.current.get(@user.token)
-    @uid = Redis.current.get(@user.uid)
-    @google_oauth_data = Redis.current.get(@user.google_oauth_data)
-    return {
-      username: @username,
-      token: @token,
-      uid: @uid,
-      google_oauth_data: @google_oauth_data
-    }
-  end
-
-  def self.search_and_save_user(user)
-    Redis.current.set(user.attributes["username"], user.attributes.to_json)
-  end
 end

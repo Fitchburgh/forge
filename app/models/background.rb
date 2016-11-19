385aeaed05228862522572844e433e85ca2fcb53
Brandon Fitch <fitchbj@gmail.com>
2016-11-19 14:46:36 -0500

    Few updates pulling for heroku push

--- app/models/background.rb
+++ app/models/background.rb
@@ -3,14 +3,6 @@ class Background < ApplicationRecord
   belongs_to :game
   validates :obj, :tags, :name, presence: true
 
-  def self.find_background_by_input(var, params)
-    var = []
-    Background.all.each do |t|
-      var << t if t.tags.include?(params) || t.name.include?(params)
-    end
-    var
-  end
-
   def self.create_background_article(var, options, auth_id)
     var = self.new_background(options, auth_id)
   end

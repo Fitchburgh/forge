ec7cb743174a82a984afaa7fd489b9f26f0c0cad
Brandon Fitch <fitchbj@gmail.com>
2016-11-21 10:14:47 -0500

    Action mailer testing in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,18 +27,20 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.delivery_method = :letter_opener
-
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => "yazua.gaming@gmail.com",
-    :password             => "robjasandnatbra",
-    :authentication       => 'plain',
-    :enable_starttls_auto => true
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => 'login',
+    :enable_starttls_auto => true,
+    :openssl_verify_mode  => 'none'
   }
 
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,14 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
-   port: 587,
-   domain: "myapp.herokuapp.com",
-   authentication: "plain",
-   enable_starttls_auto: true,
-   user_name: "fitchburg",
-   password: "robjasandnatbra"
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "login",
+    :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.

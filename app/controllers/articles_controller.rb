94555a0fc71c6cad1a06f951238804cf435f7f61
Brandon Fitch <fitchbj@gmail.com>
2016-11-23 12:12:46 -0500

    Events added to return for all game assets.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,8 +20,9 @@ class ArticlesController < ApplicationController
     @backgrounds = Background.find_background_by_game(@backgrounds, params[:game_id])
     @obstacles = Obstacle.find_obstacle_by_game(@obstacles, params[:game_id])
     @entities = Entity.find_entity_by_game(@entities, params[:game_id])
+    @events = Event.find_event_by_game(@events, params[:game_id])
 
-    @articles = { 'gameName' => @game.name, 'availableBackgrounds' => @backgrounds, 'availableObstacles' => @obstacles, 'availableEntities' => @entities }
+    @articles = { 'gameName' => @game.name, 'availableBackgrounds' => @backgrounds, 'availableObstacles' => @obstacles, 'availableEntities' => @entities, 'availableEvents' => @events }
     render json: @articles
   end
 end

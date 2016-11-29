2b8d8be36e1883eb7c3593cf79ff03fbdc13ec78
nsemmler <ndsunc@gmail.com>
2016-11-29 08:25:58 -0500

    modified to replace local database with heroku database for testing

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
-# every 1.day do
+every 1.day do
   system 'heroku pg:backups capture'
   system 'curl -o latest.dump `heroku pg:backups public-url`'
 
   system 'aws s3 cp s3://yazuapgbackup/latest.dump ../yazua_backup/' if system 'pg_restore --verbose --clean --no-acl --no-owner -h localhost -U Fitchburgh -d forge_test latest.dump'
 
   system 'aws s3 cp latest.dump s3://yazuapgbackup/'
-# end
+end

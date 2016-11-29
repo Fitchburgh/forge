97dd7fa4057ff69a2dcfeddb4f1cb72a0b28e41d
nsemmler <ndsunc@gmail.com>
2016-11-29 15:03:01 -0500

    removed plays, score, thumbnail, published, archived from games

--- spec/controllers/entities_controller_spec.rb
+++ spec/controllers/entities_controller_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe EntitiesController, type: :controller do
   before do
     @u1 = User.create!(username: 'emanresu', token: 'asdKJFKNfsdJKDSNnfdsjnf', uid: 238879432983, google_oauth_data: 'placeholder' )
     @u2 = User.create!(username: 'planetglobe', token: 'pqowiURTBVncmxPQOWI', uid: 34123432123, google_oauth_data: 'placeholder' )
-    @g1 = Game.create!( name: 'neature', tags: 'thats pretty neat', description: 'you can tell its a game from the way it is', user_id: @u1.id, info: 'lots of stuff', published: true, archived: false, score: 100, plays: 12, thumbnail: 'asdf' )
-    @g2 = Game.create!( name: 'kreature kombat', tags: 'fighting', description: 'seafare at it\'s finest', user_id: @u1.id, info: 'lots of stuff', published: true, archived: false, score: 150, plays: 123, thumbnail: 'asdf' )
-    @g3 = Game.create!( name: 'flyy', tags: 'adventure', description: 'it\'s just a maze', user_id: @u2.id, info: 'lots of stuff', published: true, archived: false, score: 100, plays: 2, thumbnail: 'asdf' )
+    @g1 = Game.create!( name: 'neature', tags: 'thats pretty neat', description: 'you can tell its a game from the way it is', user_id: @u1.id, info: 'lots of stuff' )
+    @g2 = Game.create!( name: 'kreature kombat', tags: 'fighting', description: 'seafare at it\'s finest', user_id: @u1.id, info: 'lots of stuff' )
+    @g3 = Game.create!( name: 'flyy', tags: 'adventure', description: 'it\'s just a maze', user_id: @u2.id, info: 'lots of stuff' )
     @e1 = Entity.create!( name: 'bird', tags: 'larch, animal', user_id: @u1.id, game_id: @g2.id, info: 'lots of stuff', published: true, thumbnail: 'more stuff' )
     @e2 = Entity.create!( name: 'ferret', tags: 'mongoose, weasel, mammal, animal', user_id: @u2.id, game_id: @g1.id, info: 'lots of stuff', published: true, thumbnail: 'more stuff' )
     @e3 = Entity.create!( name: 'beetle', tags: 'dung beetle, insect', user_id: @u2.id, game_id: @g1.id, info: 'lots of stuff', published: true, thumbnail: 'more stuff' )

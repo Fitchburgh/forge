22bc40656ee9b6e5e04d3d98f19a116c9e41f6c6
nsemmler <ndsunc@gmail.com>
2016-11-29 19:28:12 -0500

    removed redis comments from prelogin users load action

--- app/controllers/prelogin_users_controller.rb
+++ app/controllers/prelogin_users_controller.rb
@@ -23,19 +23,11 @@ class PreloginUsersController < ApplicationController
   end
 
   def load
-    # if Redis.current.exists(params[:name])
-    #   render json: Redis.current.get(params[:name])
-    #   @game = Game.find_by(name: params[:name])
-    #   # Redis.current.expire(@game.name, 2592000)
-    # else
-      @game = Game.find_by(name: params[:name])
-      if !@game.nil?
-        # Redis.current.set(@game.name, @game.attributes.to_json)
-        # Redis.current.expire(@game.name, 2592000)
-        render json: @game
-      else
-        render :json => { errors: 'this game does not exist' }, status: 400
-      # end
+    @game = Game.find_by(name: params[:name])
+    if !@game.nil?
+      render json: @game
+    else
+      render :json => { errors: 'this game does not exist' }, status: 400
     end
   end
 
